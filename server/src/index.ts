4cb0bf029434b0bf1b44df35c1ed32f0212ca967
dlwortmann <dannywortmann@gmail.com>
2025-08-09 17:18:23 -0500

    working to get front and back end speaking to each other, still working on fixing CORS issues.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,28 @@ import express from 'express';
 import cors from 'cors';
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    //credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get('/api/menu', (req, res) => {
-  res.json([
+
+// starter menu
+const menu = [
     { id: 1, name: 'Corned Beef Hash', price: 13.99 },
-    { id: 2, name: 'Ham Benedict', price: 12.99 }
-  ]);
+    { id: 2, name: 'Ham Benedict', price: 12.99 },
+    { id: 3, name: 'Heirloom Tomato Salad', price: 10.99 }
+]
+app.get('/api/menu', (req, res) => {
+  res.json(menu);
 });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+const PORT = 5000;
+app.listen(PORT, () => { 
+    console.log(`Server running on http://localhost:${PORT}`);
+})
